821e9574c3cb3c54955de350d1735e89d19a531b
BenjieA <60783997+BenjieA@users.noreply.github.com>
2020-03-17 10:27:48 +0000

    Update dockerset.sh

--- dockerset.sh
+++ dockerset.sh
@@ -1,8 +1,11 @@
 #! /bin/bash
+sudo apt update
 
 curl https://get.docker.com | sudo bash
 sudo usermod -aG docker $(whoami)
 sudo systemctl docker start
 sudo systemctl docker enable
+
+sudo apt install docker-compose
 
 echo "Please restart your Terminal"
