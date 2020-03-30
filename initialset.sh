2726e0cd6645d1fff4ea127ee9a4fe0b119b622d
BenjieA <60783997+BenjieA@users.noreply.github.com>
2020-03-30 23:08:23 +0100

    Update initialset.sh

--- initialset.sh
+++ initialset.sh
@@ -5,9 +5,13 @@ sudo apt install python3
 sudo apt install python-pip
 sudo apt install git
 
+cd ..
+mkdir workspace
+cd workspace
+
 sudo apt-get intall  python3-venv
 python3 -m venv venv
 source venv/bin/activate
 pip install flask
-pip3 freeze | grep -E '(Flask|Jinja)'
+pip3 freeze | grep -E '(Flask|Jinja)' > requirements.txt
 sudo apt install -y tree
